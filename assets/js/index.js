3b45541b800fd1409b9e212ba35c9d911d5cb1f3
LGit <447089649@qq.com>
2020-09-11 11:37:20 +0800

    user-pwd-reset

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,33 +4,40 @@ $(function () {
 
   //   获取登录后保存在本地的token
   //   var token = window.localStorage.getItem('token') || ''
-  $.ajax({
-    url: '/my/userinfo',
-    // 请求头设置（s不要忘）
-    // headers: {
-    //   Authorization: token,
-    // },
-    success: function (res) {
-      //   debugger  （断点调试）
-      //   console.log(res)
 
-      //   res.data.user_pic
+  getUserInfo()
 
-      var resname = res.data.nickname || res.data.username
-      $('#welcome').html(resname)
+  function getUserInfo() {
+    $.ajax({
+      url: '/my/userinfo',
+      // 请求头设置（s不要忘）
+      // headers: {
+      //   Authorization: token,
+      // },
+      success: function (res) {
+        //   debugger  （断点调试）
+        //   console.log(res)
 
-      // 存在：显示图片头像，隐藏文字头像
-      if (res.data.user_pic) {
-        $('.layui-nav-img').attr('src', res.data.user_pic).show()
-        $('.text-avatar').hide()
-      } else {
-        // 反之
-        $('.layui-nav-img').hide()
+        //   res.data.user_pic
+        console.log('success', res)
+        if (res.status === 1) return
+        var resname = res.data.nickname || res.data.username
+        $('#welcome').html(resname)
 
-        $('.text-avatar').html(resname[0].toUpperCase())
-      }
-    },
-  })
+        // 存在：显示图片头像，隐藏文字头像
+        if (res.data.user_pic) {
+          $('.layui-nav-img').attr('src', res.data.user_pic).show()
+          $('.text-avatar').hide()
+        } else {
+          // 反之
+          $('.layui-nav-img').hide()
+
+          $('.text-avatar').html(resname[0].toUpperCase())
+        }
+      },
+    })
+  }
+  window.getUserInfo = getUserInfo
 
   // 用户退出
   $('#btn-logout').click(function (e) {
