364db633f17877f5a98733abb76a5c9750e8f22b
LGit <447089649@qq.com>
2020-09-13 12:42:10 +0800

    cate-art-list

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -20,7 +20,7 @@ $(function () {
 
   $('#file').on('change', function (e) {
     // e.target获取当前的input：file这个DOM=>
-    console.log(e.target.files)
+    // console.log(e.target.files)
     // 1. 获取图片对象
     var file = e.target.files[0]
     // 2. 根据选择的文件，创建一个对应的 URL 地址：
@@ -31,6 +31,30 @@ $(function () {
       .cropper('destroy') // 销毁旧的裁剪区域
       .attr('src', newImgURL) // 重新设置图片路径
       .cropper(options) // 重新初始化裁剪区域
+  })
+
+  // 确定上传
+
+  $('#sure').on('click', function (e) {
+    e.preventDefault()
+    // 获取图片：把图片url转换成base64
+
+    var dataURL = $image
+      .cropper('getCroppedCanvas', {
+        // 创建一个 Canvas 画布
+        width: 100,
+        height: 100,
+      })
+      .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    // console.log(dataURL)
+    // 发送请求
+    $.post('/my/update/avatar', { avatar: dataURL }, function (res) {
+      if (res.status === 0) {
+        // console.log(res.message)
+        // 调用iframe形成的父页面index.html内嵌的index.js中的方法
+        window.parent.getUserInfo()
+      }
+    })
   })
 })
 
