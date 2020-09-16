360e6fe3478daf422b68b50b868f66c783d6cd4b
LGit <447089649@qq.com>
2020-09-16 13:22:30 +0800

    finish

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -48,14 +48,61 @@ $(function () {
     // 根据form数据去实例化FormData数据
     var fd = new FormData($(this)[0])
     // console.log(fd['title']);
-
     fd.append('state', state)
 
-    fd.forEach(function (v, k) {
-      console.log(k, v)
-    })
+    // 获取选择后的图片，并且利用toBlob转换成了接口要的进制数据
+    $image
+      .cropper('getCroppedCanvas', {
+        // 创建一个 Canvas 画布
+        width: 400,
+        height: 280,
+      })
+      .toBlob(function (blob) {
+        // 将 Canvas 画布上的内容，转化为文件对象
+        // 得到文件对象后，进行后续的操作
+        fd.append('cover_img', blob)
+
+        // fd.forEach(function (v, k) {
+        //   console.log(k, v)
+        // })
+
+        // form->submit->序列化->使用js把表单数据变得像json，比如key和value
+        // 发送formData这种数据类型时，需要设置下面两个属性
+        //  Content-Type:false
+        //  processData:false
+        //  解释：
+        $.ajax({
+          url: `/my/article/add`,
+          data: fd,
+          method: 'POST',
+          // 使用formData时需要设置的
+          contentType: false,
+          processData: false,
+          success: function (res) {
+            console.log(res)
+            if (res.status === 0) {
+              window.location.href = '/article/art_list.html'
+            }
+          },
+        })
+      })
   })
 })
 
 //
 // [1,2,3,4,5].forEach(function(a,b){})
+// var $image = {
+//   a:function(){
+//     return $image
+//   },
+//   b:function(){}
+// }
+// var res = $image.a()
+// res.b()
+
+// function a(){
+//   return
+// }
+// // $image.a().b()
+// var res = $image.a()
+// res.b()
