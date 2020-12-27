ec9ea9e80c56328ef072e8309f9b22d29cae178f
LGit <447089649@qq.com>
2020-12-27 09:17:21 +0800

    initProject

--- assets/lib/tinymce/tinymce_setup.js
+++ assets/lib/tinymce/tinymce_setup.js
@@ -6,7 +6,7 @@ function initEditor() {
     directionality: 'ltr',
     //语言选择中文
     language: 'zh_CN',
-    //高度为400
+    // 高度为400
     height: 300,
     statusbar: false,
     width: '100%',
@@ -15,11 +15,10 @@ function initEditor() {
       'advlist autolink link image lists charmap preview hr anchor pagebreak spellchecker',
       'searchreplace wordcount visualblocks visualchars code insertdatetime nonbreaking',
       'save table contextmenu directionality template paste textcolor',
-      'codesample imageupload',
+      'codesample imageupload'
     ],
     //工具栏的补丁按钮
-    toolbar:
-      'insertfile undo redo | \
+    toolbar: 'insertfile undo redo | \
        styleselect | \
        bold italic | \
        alignleft aligncenter alignright alignjustify | \
@@ -32,7 +31,7 @@ function initEditor() {
     //字体大小
     fontsize_formats: '10pt 12pt 14pt 18pt 24pt 36pt',
     //按tab不换行
-    nonbreaking_force_tab: true,
-    //   imageupload_url: '/user/submit-image'
+    nonbreaking_force_tab: true
+      //   imageupload_url: '/user/submit-image'
   })
 }
